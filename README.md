5abb00f615afcfb698cd40dc166143a12d9eb64f
cztqm <saezchristopher1234@gmail.com>
2026-09-16 10:02:48 -0300

    avance main

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # POO
+## Christopher Saez
+### git config --global --list
+### git config --global user.name usuario_github
+### git config --global user.email email_github
+### git add .
+### git commit -m "Paciente iniciado"
+### git push origin main
---
+++ main.py
@@ -0,0 +1,10 @@
+from paciente import Paciente
+
+def main():
+    #crear paciente con el constructor __int__
+    pl = Paciente("11.111.111.-1","Franco suarez", 40, "isapre")
+    #mostrar infrmacion del paciente
+    #__str_ es llamado automaticamente al imprimir el objeto
+    print(p1)
+if __name__ == "__main__":
+    main()
